a969064f1da9ac1f1974cc28bb6143dae5a33dff
DELL <DELL@DESKTOP-23UR82L>
2024-07-29 13:08:04 +0545

    Add project files.

---
+++ Bluetooth/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Bluetooth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Devices = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bluetooth Devices";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(518, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Scan  🔃";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Devices
+            // 
+            this.Devices.FormattingEnabled = true;
+            this.Devices.Location = new System.Drawing.Point(362, 91);
+            this.Devices.Name = "Devices";
+            this.Devices.Size = new System.Drawing.Size(253, 290);
+            this.Devices.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(9, 83);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 309);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 399);
+            this.Controls.Add(this.Devices);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox Devices;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Bluetooth/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Bluetooth;
+using InTheHand.Net.Sockets;
+
+namespace Bluetooth
+{
+    public partial class Form1 : Form
+    {
+        List<BluetoothDeviceInfo> _bluetoothDevices;
+        bool _isSearchingForDevices;
+        
+        public bool IsSearchingForDevices
+        {
+            get => _isSearchingForDevices;
+            set
+            {
+                _isSearchingForDevices = value;
+                button1.Enabled = !_isSearchingForDevices;
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            _bluetoothDevices = new List<BluetoothDeviceInfo>();
+            Devices.DisplayMember = "DeviceName";
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // List<BluetoothDeviceInfo> _bluetoothDevices;
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            IsSearchingForDevices=true;
+            _bluetoothDevices.clear();
+            Devices.Controls.Clear();
+            groupBox1.Items.Clear();
+            try
+            {
+                var bluetoothDevices = await SearchDevicesAsync();
+                _bluetoothDevices.AddRange(bluetoothDevices);
+            }
+            IsSeachingForDevices = false;
+        }
+    }
+}
